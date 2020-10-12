a9792381505b14b43b31d2b04aab051fbedcc98e
apkil <64503136+apkil@users.noreply.github.com>
2020-10-12 03:24:34 -0300

    arruma data e hora

--- database/database.js
+++ database/database.js
@@ -6,8 +6,14 @@ const connection = new Sequelize('bd5s','root','123456',{
     port: 3306,
     dialect: 'mysql',
     dialectOptions: {
+      typeCast: function (field, next) {
+        if(field.type === 'DATETIME') {
+          return field.string()
+        }
+          return next()
+        }
       //timezone: "+03:00",
-      useUTC: false, // for reading from database
+      //useUTC: false, // for reading from database
     },
     timezone: '-03:00', // for writing to database
 }); 
